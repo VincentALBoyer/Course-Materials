cd9ae28c08bc0a6192dfdb122da8219e88222688
Vincent Boyer <55854488+VincentALBoyer@users.noreply.github.com>
2024-05-30 23:29:46 -0600

    it works

--- Summer Classes/Introduction to c++/GameOfLife/GameOfLife.cpp
+++ Summer Classes/Introduction to c++/GameOfLife/GameOfLife.cpp
@@ -15,6 +15,10 @@ struct Grid {
 
 Grid initEmptyGrid(int size);
 
+Grid copyGrid(const Grid& G);
+
+void deleteGrid(Grid& G);
+
 void printGrid(const Grid& G);
 
 void updateGridState(Grid& G);
@@ -26,7 +30,19 @@ int main(int argc, char argv[]) {
 
 	Grid G = initEmptyGrid(20);
 
-	printGrid(G);
+	G.states[1][1] = State::ALIVE;
+	G.states[1][3] = State::ALIVE;
+	G.states[2][2] = State::ALIVE;
+	G.states[2][3] = State::ALIVE;
+	G.states[3][2] = State::ALIVE;
+
+
+	while (G.gen++ < 100) {
+		printGrid(G);
+		updateGridState(G);
+		Sleep(50);
+	}
+	
 
 	return 0;
 }
@@ -52,6 +68,34 @@ void printGrid(const Grid& G)
 
 }
 
+void updateGridState(Grid& G)
+{
+	Grid tempG = copyGrid(G);
+	for (int i = 0; i < G.size; ++i) for (int j = 0; j < G.size; ++j){
+		int n = countAlives(tempG, i, j);
+		if (tempG.states[i][j] == State::ALIVE && (n == 2 || n == 3))
+			G.states[i][j] = State::ALIVE;
+		else if (tempG.states[i][j] == State::DEAD && n == 3)
+			G.states[i][j] = State::ALIVE;
+		else
+			G.states[i][j] = State::DEAD;
+	}
+
+	deleteGrid(tempG);
+}
+
+int countAlives(const Grid& G, int i, int j)
+{
+	int nalives = 0;
+	for (int k = -1; k <= 1; ++k) for (int l = -1; l <= 1; ++l) if(k!=0 || l!=0){
+		
+		int x = (k + i + G.size) % G.size;
+		int y = (l + j + G.size) % G.size;
+		nalives += (int)(G.states[x][y]==State::ALIVE);
+	}
+	return nalives;
+}
+
 Grid initEmptyGrid(int size)
 {
 	Grid G;
@@ -63,4 +107,19 @@ Grid initEmptyGrid(int size)
 		for (int j = 0; j < G.size; ++j) G.states[i][j] = State::DEAD;
 	}
 	return G;
+}
+
+Grid copyGrid(const Grid& G)
+{
+	Grid H = initEmptyGrid(G.size);
+	for (int i = 0; i < G.size; ++i) {
+		for (int j = 0; j < G.size; ++j) H.states[i][j]= G.states[i][j];
+	}
+	return H;
+}
+
+void deleteGrid(Grid& G)
+{
+	for (int i = 0; i < G.size; ++i) delete[] G.states[i];
+	delete[] G.states;
 }

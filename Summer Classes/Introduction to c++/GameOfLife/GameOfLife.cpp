c1b967f4952f22dcb7f0b37ea90ce0d9f3306e57
Vincent Boyer <55854488+VincentALBoyer@users.noreply.github.com>
2024-05-30 22:58:54 -0600

    FIrst attempt

--- Summer Classes/Introduction to c++/GameOfLife/GameOfLife.cpp
+++ Summer Classes/Introduction to c++/GameOfLife/GameOfLife.cpp
@@ -1,20 +1,66 @@
-// GameOfLife.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+﻿// Author: Mario Talevski
 #include <iostream>
+#include <iomanip>
+#include <Windows.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+enum State{ALIVE, DEAD};
+
+struct Grid {
+	int gen;
+	int size;
+	State** states;
+};
+
+Grid initEmptyGrid(int size);
+
+void printGrid(const Grid& G);
+
+void updateGridState(Grid& G);
+
+int countAlives(const Grid& G, int i, int j);
+
+
+int main(int argc, char argv[]) {
+
+	Grid G = initEmptyGrid(20);
+
+	printGrid(G);
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void printGrid(const Grid& G)
+{
+	system("CLS");
+	int w = 2;
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	for (int i = 0; i < G.size; ++i) {
+		for (int j = 0; j < G.size; ++j) {
+			if (G.states[i][j] == State::DEAD) 
+				SetConsoleTextAttribute(handle, 15);
+			else 
+				SetConsoleTextAttribute(handle, 10);
+			
+			cout << setw(w) << (char)254u;
+		}
+		cout << endl;
+	}
+	SetConsoleTextAttribute(handle, 15);
+	cout << "Gen: " << G.gen << endl;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+}
+
+Grid initEmptyGrid(int size)
+{
+	Grid G;
+	G.size = size;
+	G.gen = 0;
+	G.states = new State * [size];
+	for (int i = 0; i < G.size; ++i) {
+		G.states[i] = new State[G.size];
+		for (int j = 0; j < G.size; ++j) G.states[i][j] = State::DEAD;
+	}
+	return G;
+}

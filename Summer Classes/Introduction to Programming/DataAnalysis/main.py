55d12140a0d17ee1f32baecae68ce7b3f310ee7b
Vincent Boyer <55854488+VincentALBoyer@users.noreply.github.com>
2024-07-10 22:54:10 -0600

    Playing around

--- Summer Classes/Introduction to Programming/DataAnalysis/main.py
+++ Summer Classes/Introduction to Programming/DataAnalysis/main.py
@@ -2,15 +2,22 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import pandas as pd
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main() -> object:
+    csvfile = "tips.csv"
+    df = pd.read_csv(csvfile, on_bad_lines="warn")
+    print(df.head())
+    df.info()
+    print(df.isna().sum())
+    print(df.describe())
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
